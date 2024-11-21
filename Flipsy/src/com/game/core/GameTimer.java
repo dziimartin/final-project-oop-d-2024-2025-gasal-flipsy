5c4d2a6231b3269787949e3a46cb871fbad460b7
Rosidah Darman <163227259+Osshiii@users.noreply.github.com>
2024-11-22 00:02:39 +0700

    Update GameTimer.java

--- Flipsy/src/com/game/core/GameTimer.java
+++ Flipsy/src/com/game/core/GameTimer.java
@@ -4,57 +4,34 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class GameTimer {
-    private int timeInSeconds;
-    private Timer timer;
-    private TimerListener listener; // Listener untuk update waktu
-    private boolean isPaused; // Status pause
+    private int timeInSeconds; 
+    private Timer timer;       
+    private TimerListener listener; 
+    private boolean isRunning; 
 
     public GameTimer() {
         this.timeInSeconds = 0;
-        this.isPaused = false;
+        this.isRunning = false;
+    }
+
+    public void setListener(TimerListener listener) {
+        this.listener = listener;
     }
 
     public void start() {
-        if (timer != null) {
-            stop();
+        if (isRunning) {
+            return; 
         }
 
+        isRunning = true;
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                timeInSeconds++;
-                System.out.println("Time: " + timeInSeconds + " seconds");
-                notifyListener(); // Beri tahu listener
+                timeInSeconds++; 
+                notifyListener(); 
             }
-        }, 1000, 1000);
-    }
-
-    public void stop() {
-        if (timer != null) {
-            timer.cancel();
-            timer = null;
-        }
-    }
-
-    public void reset() {
-        stop();
-        timeInSeconds = 0;
-        isPaused = false;
-    }
-
-    public void pause() {
-        if (timer != null) {
-            isPaused = true;
-            stop();
-        }
-    }
-
-    public void resume() {
-        if (isPaused) {
-            isPaused = false;
-            start();
-        }
+        }, 1000, 1000); 
     }
 
     public int getTimeInSeconds() {
@@ -64,21 +41,20 @@ public class GameTimer {
     public String getFormattedTime() {
         int minutes = timeInSeconds / 60;
         int seconds = timeInSeconds % 60;
-        return String.format("%02d:%02d", minutes, seconds);
-    }
-    
-
-    public void setTimerListener(TimerListener listener) {
-        this.listener = listener;
+        return String.format("%02d:%02d", minutes, seconds); 
     }
 
     private void notifyListener() {
         if (listener != null) {
-            listener.onTimeUpdate(getFormattedTime());
+            listener.onTimeUpdate(timeInSeconds); 
         }
     }
 
+    public boolean isRunning() {
+        return isRunning;
+    }
+
     public interface TimerListener {
-        void onTimeUpdate(String formattedTime);
+        void onTimeUpdate(int seconds);
     }
 }
